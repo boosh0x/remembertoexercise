9921d9d10cfb5243e3d3e6d43da2d0c7346b077f
Boosh <boosh0x@protonmail.com>
2023-04-03 22:43:07 -1000

    Edit buttons, padding, and text

--- reminder_to_exercise.py
+++ reminder_to_exercise.py
@@ -1,96 +1,96 @@
-import time
 import threading
+import time
 from playsound import playsound
 from PyObjCTools import AppHelper
-from Cocoa import NSApplication, NSApp, NSWindow, NSButton, NSTextField, NSObject, NSMakeRect, NSBackingStoreBuffered, NSWindowStyleMaskTitled, NSWindowStyleMaskClosable, NSWindowStyleMaskMiniaturizable, NSDate
+from AppKit import NSApp, NSApplication, NSWindow, NSButton, NSTextField, NSColor, NSTextAlignment, NSFont, NSFontManager, NSFontPanel
 
-class StretchReminderApp(NSObject):
-    def applicationDidFinishLaunching_(self, notification):
-        styleMask = (
-            NSWindowStyleMaskTitled |
-            NSWindowStyleMaskClosable |
-            NSWindowStyleMaskMiniaturizable
+class ReminderApp(NSApplication):
+
+    def finishLaunching(self):
+        self.main_window = NSWindow.alloc().initWithContentRect_styleMask_backing_defer_(
+            ((300, 300), (320, 200)),
+            15, 2, 0
         )
-        frame = NSMakeRect(0, 0, 300, 200)
-        self.window = NSWindow.alloc().initWithContentRect_styleMask_backing_defer_(
-            frame,
-            styleMask,
-            NSBackingStoreBuffered,
-            False
-        )
-        self.window.setTitle_("Reminder to Exercise")
-        self.window.center()
+        self.main_window.setBackgroundColor_(NSColor.lightGrayColor())
 
-        self.timer_text = NSTextField.alloc().initWithFrame_(NSMakeRect(100, 140, 100, 24))
-        self.timer_text.setStringValue_("01:00:00")
-        self.window.contentView().addSubview_(self.timer_text)
+        self.timer_label = NSTextField.alloc().initWithFrame_(((20, 150), (280, 40)))
+        self.timer_label.setEditable_(False)
+        self.timer_label.setBordered_(False)
+        self.timer_label.setBackgroundColor_(NSColor.lightGrayColor())
+        self.timer_label.setFont_(NSFont.boldSystemFontOfSize_(36))
+        self.timer_label.setAlignment_(2)  # 2 corresponds to NSTextAlignmentCenter
+        self.main_window.contentView().addSubview_(self.timer_label)
 
-        self.start_button = NSButton.alloc().initWithFrame_(NSMakeRect(40, 60, 80, 32))
-        self.start_button.setTitle_("Start")
-        self.start_button.setTarget_(self)
-        self.start_button.setAction_("start:")
-        self.window.contentView().addSubview_(self.start_button)
+        button_start = NSButton.alloc().initWithFrame_(((20, 100), (60, 40)))
+        button_start.setTitle_("Start")
+        button_start.setFont_(NSFont.systemFontOfSize_(14))
+        button_start.setTarget_(self)
+        button_start.setAction_("start:")
+        self.main_window.contentView().addSubview_(button_start)
 
-        self.pause_button = NSButton.alloc().initWithFrame_(NSMakeRect(140, 60, 80, 32))
-        self.pause_button.setTitle_("Pause")
-        self.pause_button.setTarget_(self)
-        self.pause_button.setAction_("pause:")
-        self.window.contentView().addSubview_(self.pause_button)
+        button_pause = NSButton.alloc().initWithFrame_(((90, 100), (60, 40)))
+        button_pause.setTitle_("Pause")
+        button_pause.setFont_(NSFont.systemFontOfSize_(14))
+        button_pause.setTarget_(self)
+        button_pause.setAction_("pause:")
+        self.main_window.contentView().addSubview_(button_pause)
 
-        self.reset_button = NSButton.alloc().initWithFrame_(NSMakeRect(40, 20, 80, 32))
-        self.reset_button.setTitle_("Reset")
-        self.reset_button.setTarget_(self)
-        self.reset_button.setAction_("reset:")
-        self.window.contentView().addSubview_(self.reset_button)
+        button_reset = NSButton.alloc().initWithFrame_(((160, 100), (60, 40)))
+        button_reset.setTitle_("Reset")
+        button_reset.setFont_(NSFont.systemFontOfSize_(14))
+        button_reset.setTarget_(self)
+        button_reset.setAction_("reset:")
+        self.main_window.contentView().addSubview_(button_reset)
 
-        self.quit_button = NSButton.alloc().initWithFrame_(NSMakeRect(140, 20, 80, 32))
-        self.quit_button.setTitle_("Quit")
-        self.quit_button.setTarget_(self)
-        self.quit_button.setAction_("quit:")
-        self.window.contentView().addSubview_(self.quit_button)
+        button_quit = NSButton.alloc().initWithFrame_(((230, 100), (60, 40)))
+        button_quit.setTitle_("Quit")
+        button_quit.setFont_(NSFont.systemFontOfSize_(14))
+        button_quit.setTarget_(self)
+        button_quit.setAction_("terminate:")
+        self.main_window.contentView().addSubview_(button_quit)
 
-        self.window.orderFrontRegardless()
-        self.timer = None
+        self.timer_seconds = 10
+        self.update_timer()
+        self.timer_event = threading.Event()
 
-    def start_(self, sender):
-        if self.timer is None or not self.timer.is_alive():
-            self.timer = threading.Thread(target=self.countdown)
-            self.timer.start()
+        self.main_window.makeKeyAndOrderFront_(None)
+        self.main_window.setTitle_("Reminder to Exercise")
+        self.activateIgnoringOtherApps_(True)
 
-    def pause_(self, sender):
-        if self.timer is not None:
-            self.timer_active = False
-            self.timer.join()
-            self.timer = None
+        self.timer_thread = None
 
-    def reset_(self, sender):
-        self.pause_(None)
-        self.timer_text.setStringValue_("01:00:00")
 
-    def quit_(self, sender):
-        self.pause_(None)
-        NSApp.terminate_(self)
+    def update_timer(self):
+        minutes, seconds = divmod(self.timer_seconds, 60)
+        self.timer_label.setStringValue_("{:02d}:{:02d}".format(minutes, seconds))
 
-    def countdown(self):
-        self.timer_active = True
-        total_seconds = 3600  # Change this value to a few seconds for testing
-        while total_seconds > 0 and self.timer_active:
-            minutes, seconds = divmod(total_seconds, 60)
-            hours, minutes = divmod(minutes, 60)
-            time_str = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
-            self.timer_text.performSelectorOnMainThread_withObject_waitUntilDone_("setStringValue:", time_str, True)
+    def timer_loop(self):
+        while not self.timer_event.is_set() and self.timer_seconds > 0:
             time.sleep(1)
-            total_seconds -= 1
-
-        if self.timer_active:
+            self.timer_seconds -= 1
+            self.performSelectorOnMainThread_withObject_waitUntilDone_(self.update_timer, None, False)
+        if not self.timer_event.is_set():
             playsound("./exercise.mp3")
 
+    def start_(self, sender):
+        if self.timer_thread and self.timer_thread.is_alive():
+            return
+        self.timer_event.clear()
+        self.timer_thread = threading.Thread(target=self.timer_loop)
+        self.timer_thread.start()
 
-def main():
-    app = NSApplication.sharedApplication()
-    delegate = StretchReminderApp.alloc().init()
-    app.setDelegate_(delegate)
-    AppHelper.runEventLoop()
+    def pause_(self, sender):
+        if self.timer_thread.is_alive():
+            self.timer_event.set()
+            self.timer_thread.join()
+
+    def reset_(self, sender):
+        self.pause_(sender)
+        self.timer_seconds = 10
+        self.update_timer()
 
 if __name__ == "__main__":
-    main()
+    app = ReminderApp.sharedApplication()
+    app.setDelegate_(app)
+    AppHelper.runEventLoop()
+
